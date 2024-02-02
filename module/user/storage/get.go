1d74ea63903e54f122d6fe34c65b507a5478401d
trankhanh040147 <trankhanh040147@gmail.com>
2024-02-02 14:50:45 +0700

    user storage - get

---
+++ module/user/storage/get.go
@@ -0,0 +1,29 @@
+package storage
+
+import (
+	"context"
+	"go-200lab-g09/common"
+	"go-200lab-g09/module/user/model"
+
+	"gorm.io/gorm"
+)
+
+func (s *sqlStore) FindUser(ctx context.Context, conditions map[string]interface{}, moreInfo ...string) (*model.User, error) {
+	db := s.db.Table(model.User{}.TableName())
+
+	for i := range moreInfo {
+		db = db.Preload(moreInfo[i])
+	}
+
+	var user model.User
+
+	if err := db.Where(conditions).First(&user).Error; err != nil {
+		if err == gorm.ErrRecordNotFound {
+			return nil, common.RecordNotFound
+		}
+
+		return nil, common.ErrDB(err)
+	}
+
+	return &user, nil
+}
