a5589277e73b05e2a793640017e039a5a9158b7b
trankhanh040147 <trankhanh040147@gmail.com>
2024-02-05 20:58:24 +0700

    Update FakeID SQLModel

--- common/sql_model.go
+++ common/sql_model.go
@@ -3,7 +3,13 @@ package common
 import "time"
 
 type SQLModel struct {
-	Id        int        `json:"id" gorm:"column:id;"`
+	Id        int        `json:"-" gorm:"column:id;"`
+	FakeID    *UID       `json:"id" gorm:"-"`
 	CreatedAt *time.Time `json:"created_at" gorm:"column:created_at;"`
 	UpdatedAt *time.Time `json:"updated_at,omitempty" gorm:"column:updated_at;"`
+}
+
+func (sql *SQLModel) Mask(dbType DbType) {
+	uid := NewUID(uint32(sql.Id), int(dbType), 1)
+	sql.FakeID = &uid
 }
