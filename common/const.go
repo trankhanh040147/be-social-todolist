80a55e7105680ed6b03c32a8fc6cbeb5ab2dee37
trankhanh040147 <trankhanh040147@gmail.com>
2024-02-05 20:58:00 +0700

    create SimpleUser,  add DbType

--- common/const.go
+++ common/const.go
@@ -6,6 +6,13 @@ import (
 
 const CurrentUser = "current_user"
 
+type DbType int
+
+const (
+	DbTypeItem DbType = 1
+	DbTypeUser DbType = 2
+)
+
 func Recovery() {
 	if r := recover(); r != nil {
 		fmt.Println("Recovered:", r)
