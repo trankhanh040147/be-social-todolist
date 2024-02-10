376c2a17a577b36f835c144e47fca912fbe43c67
trankhanh040147 <trankhanh040147@gmail.com>
2024-02-11 04:14:52 +0700

    Revert "Revert "Revert "Revert "SDK: Execute main""""
    This reverts commit 764b6afaa16fd6b10d82c8a29a9e3eb8dcf20d82.

--- main.go
+++ main.go
@@ -1,90 +1,80 @@
 package main
 
 import (
-	"encoding/json"
-	"go-200lab-g09/common"
-	"go-200lab-g09/component/tokenprovider/jwt"
-	"go-200lab-g09/middleware"
-	"go-200lab-g09/module/item/model"
-	ginitem "go-200lab-g09/module/item/transport/gin"
-	"go-200lab-g09/module/upload"
-	"go-200lab-g09/module/user/storage"
-	ginuser "go-200lab-g09/module/user/transport/gin"
-	"log"
-	"net/http"
-
-	"github.com/gin-gonic/gin"
-	"gorm.io/driver/mysql"
-	"gorm.io/gorm"
+	"go-200lab-g09/cmd"
 )
 
 func main() {
-
-	// refer https://github.com/go-sql-driver/mysql#dsn-data-source-name for details
-	// DB_CONNECTION from launch.json
-	// dsn := os.Getenv("DB_CONNECTION")
-	dsn := "root:my-secret-pw@tcp(127.0.0.1:3309)/social-todo-list?charset=utf8mb4&parseTime=True&loc=Local"
-	// systemSecret := os.Getenv("SECRET")
-	systemSecret := "iTaskSecret2024"
-
-	db, err := gorm.Open(mysql.Open(string(dsn)), &gorm.Config{})
-	if err != nil {
-		log.Fatalln(err)
-	}
-	db = db.Debug()
-	log.Println("DB Connection: ", db)
-
-	// -----------------------------------------------
-	authStore := storage.NewSQLStore(db)
-	tokenProvider := jwt.NewTokenJwtProvider("jwt", systemSecret)
-	middlewareAuth := middleware.RequiredAuth(authStore, tokenProvider)
-
-	jsString := `{"id":1,"title":"Belajar Golang","description":"Belajar Golang untuk membuat API","status":"Active","created_at":"2021-10-13T15:04:05Z","updated_at":"2021-10-13T15:04:05Z"}`
-	var item2 model.TodoItem
-	if err := json.Unmarshal([]byte(jsString), &item2); err != nil {
-		log.Fatalln(err)
-	}
-	log.Println(item2)
-
-	// -----------------------------------------------
-
-	// > set up HTTP routes for a RESTful API.
-	// >> creates a new Gin router with default middleware. The default middleware includes logging and recovery middleware, which logs all requests and recovers from any panics, respectively.
-	r := gin.Default()
-	r.Use(middleware.Recover())
-
-	r.Static("/static", "./static")
-	// >> creates a new route group. All routes defined under this group will have the prefix /api/v1.
-	v1 := r.Group("/api/v1")
-	{
-		// >> creates a new route handler /upload
-		v1.PUT("/upload", upload.Upload(db))
-
-		v1.POST("/register", ginuser.Register(db))
-		v1.POST("/login", ginuser.Login(db, tokenProvider))
-		v1.GET("/profile", middlewareAuth, ginuser.Profile())
-
-		// >> all routes defined under this group will have the prefix /api/v1/items.
-		items := v1.Group("items", middlewareAuth)
-		{
-			items.POST("", ginitem.CreateItem(db))
-			items.GET("", ginitem.ListItem(db))
-			items.GET("/:id", ginitem.GetItem(db))
-			items.PATCH("/:id", ginitem.UpdateItem(db))
-			items.DELETE("/:id", ginitem.DeleteItem((db)))
-		}
-	}
-
-	// > define a route handler / that returns a JSON response with a message property set to pong.
-	r.GET("/ping", func(c *gin.Context) {
-		c.JSON(http.StatusOK, common.SimpleSuccessResponse("pong"))
-	})
-
-	// > run the application on port 3000.
-	if err := r.Run(":3000"); err != nil {
-		log.Fatalln(err)
-	}
-
-	// fmt.Println("Hello, World!")
-	// fmt.Println(os.Getenv("APP_NAME"))
+	cmd.Execute()
 }
+
+// func main() {
+
+// 	// refer https://github.com/go-sql-driver/mysql#dsn-data-source-name for details
+// 	// DB_CONNECTION from launch.json
+// 	// dsn := os.Getenv("DB_CONNECTION")
+// 	dsn := "root:my-secret-pw@tcp(127.0.0.1:3309)/social-todo-list?charset=utf8mb4&parseTime=True&loc=Local"
+// 	// systemSecret := os.Getenv("SECRET")
+// 	systemSecret := "iTaskSecret2024"
+
+// 	db, err := gorm.Open(mysql.Open(string(dsn)), &gorm.Config{})
+// 	if err != nil {
+// 		log.Fatalln(err)
+// 	}
+// 	db = db.Debug()
+// 	log.Println("DB Connection: ", db)
+
+// 	// -----------------------------------------------
+// 	authStore := storage.NewSQLStore(db)
+// 	tokenProvider := jwt.NewTokenJwtProvider("jwt", systemSecret)
+// 	middlewareAuth := middleware.RequiredAuth(authStore, tokenProvider)
+
+// 	jsString := `{"id":1,"title":"Belajar Golang","description":"Belajar Golang untuk membuat API","status":"Active","created_at":"2021-10-13T15:04:05Z","updated_at":"2021-10-13T15:04:05Z"}`
+// 	var item2 model.TodoItem
+// 	if err := json.Unmarshal([]byte(jsString), &item2); err != nil {
+// 		log.Fatalln(err)
+// 	}
+// 	log.Println(item2)
+
+// 	// -----------------------------------------------
+
+// 	// > set up HTTP routes for a RESTful API.
+// 	// >> creates a new Gin router with default middleware. The default middleware includes logging and recovery middleware, which logs all requests and recovers from any panics, respectively.
+// 	r := gin.Default()
+// 	r.Use(middleware.Recover())
+
+// 	r.Static("/static", "./static")
+// 	// >> creates a new route group. All routes defined under this group will have the prefix /api/v1.
+// 	v1 := r.Group("/api/v1")
+// 	{
+// 		// >> creates a new route handler /upload
+// 		v1.PUT("/upload", upload.Upload(db))
+
+// 		v1.POST("/register", ginuser.Register(db))
+// 		v1.POST("/login", ginuser.Login(db, tokenProvider))
+// 		v1.GET("/profile", middlewareAuth, ginuser.Profile())
+
+// 		// >> all routes defined under this group will have the prefix /api/v1/items.
+// 		items := v1.Group("items", middlewareAuth)
+// 		{
+// 			items.POST("", ginitem.CreateItem(db))
+// 			items.GET("", ginitem.ListItem(db))
+// 			items.GET("/:id", ginitem.GetItem(db))
+// 			items.PATCH("/:id", ginitem.UpdateItem(db))
+// 			items.DELETE("/:id", ginitem.DeleteItem((db)))
+// 		}
+// 	}
+
+// 	// > define a route handler / that returns a JSON response with a message property set to pong.
+// 	r.GET("/ping", func(c *gin.Context) {
+// 		c.JSON(http.StatusOK, common.SimpleSuccessResponse("pong"))
+// 	})
+
+// 	// > run the application on port 3000.
+// 	if err := r.Run(":3000"); err != nil {
+// 		log.Fatalln(err)
+// 	}
+
+// 	// fmt.Println("Hello, World!")
+// 	// fmt.Println(os.Getenv("APP_NAME"))
+// }
