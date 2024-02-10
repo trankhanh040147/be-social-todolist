db0f7a7f3a9be77e8dcbf7ef71e35497cc5cc1d5
trankhanh040147 <trankhanh040147@gmail.com>
2024-02-11 02:07:52 +0700

    Revert "SDK: rm component, add plugins"
    This reverts commit 4f72ae1bc896c763802425d21a7709b376578e9b.

--- component/tokenprovider/jwt/jwt.go
+++ component/tokenprovider/jwt/jwt.go
@@ -1,57 +1,21 @@
 package jwt
 
 import (
-	"errors"
-	"flag"
 	"fmt"
-	"time"
-
 	"go-200lab-g09/common"
-	"go-200lab-g09/plugin/tokenprovider"
+	"go-200lab-g09/component/tokenprovider"
+	"time"
 
 	"github.com/dgrijalva/jwt-go"
 )
 
 type jwtProvider struct {
-	name   string
+	prefix string
 	secret string
 }
 
-func NewJWTProvider(name string) *jwtProvider {
-	return &jwtProvider{name: name}
-}
-
-func (p *jwtProvider) GetPrefix() string {
-	return p.Name()
-}
-
-func (p *jwtProvider) Get() interface{} {
-	return p
-}
-
-func (p *jwtProvider) Name() string {
-	return p.name
-}
-
-func (p *jwtProvider) InitFlags() {
-	flag.StringVar(&p.secret, "jwt-secret", common.GenSalt(40), "Secret key for generating JWT")
-}
-
-func (p *jwtProvider) Configure() error {
-	return nil
-}
-
-func (p *jwtProvider) Run() error {
-	return nil
-}
-
-func (p *jwtProvider) Stop() <-chan bool {
-	c := make(chan bool)
-	go func() {
-		c <- true
-	}()
-
-	return c
+func NewTokenJwtProvider(prefix string, secret string) *jwtProvider {
+	return &jwtProvider{prefix: prefix, secret: secret}
 }
 
 type myClaims struct {
@@ -74,6 +38,8 @@ func (j *jwtProvider) SecretKey() string {
 }
 
 func (j *jwtProvider) Generate(data tokenprovider.TokenPayload, expiry int) (tokenprovider.Token, error) {
+	// generate the JWT
+
 	now := time.Now()
 
 	t := jwt.NewWithClaims(jwt.SigningMethodHS256, myClaims{
@@ -88,45 +54,45 @@ func (j *jwtProvider) Generate(data tokenprovider.TokenPayload, expiry int) (tok
 		},
 	})
 
+	fmt.Println("t: ", t)
+
 	myToken, err := t.SignedString([]byte(j.secret))
+
+	fmt.Println("myToken: ", myToken)
+	fmt.Println("err: ", err)
+
 	if err != nil {
 		return nil, err
 	}
 
+	// return the token
 	return &token{
 		Token:   myToken,
 		Expiry:  expiry,
 		Created: now,
 	}, nil
+
 }
 
-func (j *jwtProvider) Validate(mytoken string) (tokenprovider.TokenPayload, error) {
-	keyFunc := func(token *jwt.Token) (interface{}, error) {
-		_, ok := token.Method.(*jwt.SigningMethodHMAC)
-		if !ok {
-			return nil, tokenprovider.ErrInvalidToken
-		}
+func (j *jwtProvider) Validate(myToken string) (tokenprovider.TokenPayload, error) {
+	res, err := jwt.ParseWithClaims(myToken, &myClaims{}, func(token *jwt.Token) (interface{}, error) {
 		return []byte(j.secret), nil
-	}
+	})
 
-	jwtToken, err := jwt.ParseWithClaims(mytoken, &myClaims{}, keyFunc)
 	if err != nil {
-		verr, ok := err.(*jwt.ValidationError)
-		if ok && errors.Is(verr.Inner, tokenprovider.ErrExpiredToken) {
-			return nil, tokenprovider.ErrExpiredToken
-		}
-
 		return nil, tokenprovider.ErrInvalidToken
 	}
 
-	if !jwtToken.Valid {
+	if !res.Valid {
 		return nil, tokenprovider.ErrInvalidToken
 	}
 
-	claims, ok := jwtToken.Claims.(*myClaims)
+	claims, ok := res.Claims.(*myClaims)
+
 	if !ok {
 		return nil, tokenprovider.ErrInvalidToken
 	}
 
+	//return the token
 	return claims.Payload, nil
 }
